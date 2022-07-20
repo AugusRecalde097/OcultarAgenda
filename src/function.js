d99d375c1dd37dd26da6165e3ef5f45aaed7ea50
Anabel12veron <ana15bel.veron@gmail.com>
2022-07-20 10:29:06 -0300

    Agregado de combinación de teclas

--- src/function.js
+++ src/function.js
@@ -1,8 +1,22 @@
+document.addEventListener("keydown", function(e) {
+  if (e.altKey && String.fromCharCode(e.keyCode) == 'C')
+
+  {
+    let msg ={
+      txt: "ocultar"
+    }
+
+    goMessage(msg, false, false)
+
+  }
+});
+
 console.log("content-script");
 
 chrome.runtime.onMessage.addListener(goMessage);
 
 function goMessage(msg, sender, sendResponse) {
+  //alert(msg.txt);
   if (msg.txt == "ocultar") {
     var side = document.querySelector("#side").parentElement;
 
